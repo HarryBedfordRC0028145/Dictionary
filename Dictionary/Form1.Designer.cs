0f14130b82a2d6e4ec05cbdeff09dffbea48102d
Harry Bedford <RC0028145@student.riversidecollege.ac.uk>
2018-01-31 11:54:20 +0000

    Add project files.

---
+++ Dictionary/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(2, 89);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.Text = "0";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(357, 88);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 2;
+            this.comboBox5.SelectedIndexChanged += new System.EventHandler(this.comboBox5_SelectedIndexChanged);
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Location = new System.Drawing.Point(198, 88);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(121, 21);
+            this.comboBox6.TabIndex = 3;
+            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(510, 262);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ComboBox comboBox6;
+    }
+}
+
---
+++ Dictionary/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    public partial class Form1 : Form
+    {
+
+        Dictionary<string, double> prefixes = new Dictionary<string, double>()
+            {
+                {"nm", 1e-9 },
+                {"um", 1e-6 },
+                {"mm", 1e-3 },
+                {"cm", 1e-2 },
+                {"m", 1 },
+                {"km", 1e-3 },
+                {"Mm", 1e-6 },
+                {"Gm", 1e-9 }
+            };
+            {
+                {"nm", 1e-18 },
+                {"um", 1e-12 },
+                {"mm", 1e-6 },
+                {"cm", 1e-4 },
+                {"m", 2 },
+                {"km", 1e-6},
+                {"Mm", 1e-12},
+                {"Gm", 1e-18}
+            };
+
+
+            public Form1()
+            {
+                InitializeComponent();
+                foreach (string k in prefixes.Keys)
+                {
+                    comboBox6.Items.Add(k);
+                    comboBox5.Items.Add(k);
+                }
+                comboBox6.SelectedIndex = 4;
+                comboBox5.SelectedIndex = 4;
+                Update();
+            }
+
+            void update()
+            {
+                try
+                {
+                    double d = double.Parse(textBox3.Text);
+                    double d2 = d * prefixes[comboBox6.Text] / prefixes[comboBox5.Text];
+                    label2.Text = d2.ToString();
+                }
+                catch
+                {
+                    label2.Text = "";
+                }
+
+            }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox6_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+
+        private void comboBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            update();
+        }
+    }
+}
